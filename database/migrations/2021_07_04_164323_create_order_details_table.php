1730f441fe65e7845a636d52545bad89efeee8aa
engdoaa89 <eng.doaa.2009@hotmail.com>
2021-07-04 21:01:47 +0300

    order_details_migrate

---
+++ database/migrations/2021_07_04_164323_create_order_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->string('title');
+            $table->string('type');
+            $table->double('quantity');
+            $table->double('price');
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
